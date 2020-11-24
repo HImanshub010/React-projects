10cba9c2d5f782422c2d08362e092471f56762ac
HImanshub010 <himanshub010@gmail.com>
2020-11-25 00:18:47 +0530

    added review mini project in react

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Tour = ({id, name, info, image, price, removeTour}) => {
+const Tour = ({ id, name, info, image, price, removeTour }) => {
   console.log('>>>>>>>>>>', info)
   const [readMore, setReadMore] = useState(false)
-  return <article className='single-tour'>
-    <img src={image} alt={name}/>
-    <footer>
-      <div className="tour-info">
-      <h4>{name}</h4>
-<h4 className='tour-price'>${price}</h4>
-      </div>
-      <p>{readMore ? info : `${info.substring(0,200)}...`}
-<button onClick= {(() => setReadMore(!readMore))}>{readMore ? 'show less': 'read More'}</button></p>
-      <button className='delete-btn' onClick= {() => removeTour(id)}>not interested</button>
-    </footer>
-  </article>
-  ;
-};
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read More'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  )
+}
 
-export default Tour;
+export default Tour
---
+++ 3-reviews/setup/src/Review.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
+const Review = () => {
+  const [index, setIndex] = useState(0)
+  const { id, name, job, image, text } = people[index]
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <FaChevronLeft
+          className='next-btn'
+          onClick={() => setIndex((index + 1) % people.length)}
+        />
+        <FaChevronRight
+          className='prev-btn'
+          onClick={() =>
+            setIndex(
+              (index - 1 >= 0 ? index - 1 : people.length - 1) % people.length
+            )
+          }
+        />
+        <button
+          className='random-btn'
+          onClick={() => setIndex(Math.floor(Math.random() * people.length))}
+        >
+          Suprise Me
+        </button>
+      </div>
+    </article>
+  )
+}
+
+export default Review
